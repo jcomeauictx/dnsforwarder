0fab89e5bfceb10370b1e483a51790697d4af876
John Comeau <jc@unternet.net>
2026-03-25 15:47:28 -0700

    linted and tests passed

--- hostsfile.py
+++ hostsfile.py
@@ -5,7 +5,7 @@ read in hosts file as dict
 maps names with IP addresses to override DNS queries and add
 custom TLD resolution.
 '''
-import sys, os, logging
+import sys, logging  # pylint: disable=multiple-imports
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
@@ -13,10 +13,10 @@ def hostsfile():
     '''
     read in /etc/hosts and return a dict
     '''
-    # FIXME: according to hosts manpage, it is allowed to have two entries
+    # according to hosts manpage, it is allowed to have two entries
     # for each hostname, one for each version of the IP protocol (v4 and v6)
-    with open('/etc/hosts') as infile:
-        hosts = {}
+    with open('/etc/hosts', encoding='utf-8') as infile:
+        hosts = {'ipv4': {}, 'ipv6': {}}
         for line in infile:
             if '#' in line:
                 data, comment = line.split('#', 1)
@@ -27,13 +27,14 @@ def hostsfile():
             if len(parts) < 2:
                 logging.error('skipping malformed entry %r', line)
             else:
+                protocol = 'ipv6' if ':' in parts[0] else 'ipv4'
                 for hostname in parts[1:]:
-                    if hostname in hosts:
-                        logging.warn(
+                    if hostname in hosts[protocol]:
+                        logging.warning(
                             'overriding entry %s %s with %s',
-                            hostname, hosts[hostname], parts[0]
+                            hostname, hosts[protocol][hostname], parts[0]
                         )
-                    hosts[hostname] = parts[0]
+                    hosts[protocol][hostname] = parts[0]
     return hosts
 
 if __name__ == '__main__':
